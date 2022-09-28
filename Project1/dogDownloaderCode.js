7c06d77f678338694c91f8c009a99585f1d412e7
marcoalexander <timmythetom132@gmail.com>
2022-09-27 20:19:40 -0500

    finished part 1, move to part 2 of project

--- Project1/dogDownloaderCode.js
+++ Project1/dogDownloaderCode.js
@@ -16,7 +16,9 @@ the error of mp4's is that the code will capture the video URL however when i tr
 to download it, the page displays a video player but it is blank. Could the video be too large?
 */
 const puppeteer = require('puppeteer');
-const fs = require('fs/promises');
+const fsp = require('fs/promises');
+const fs = require('fs');
+const https = require('https');
 
 (async () => {
   const browser = await puppeteer.launch({headless: false});
@@ -27,17 +29,31 @@ const fs = require('fs/promises');
     //using img as a selector works, but just in case i also used document.querySelector('div[class="css-14qatfq-stack css-3mf84h"] > img') within dev tools in order to find the img
     const imageURL = await page.$eval("img", img => img.src);
 
-    console.log(imageURL + "image downloaded");
+    const file = fs.createWriteStream(imageURL.split("/").pop());
+    const request = https.get(imageURL, function(response) {
+      response.pipe(file);
 
-    const imagePage = await page.goto(imageURL, {waitUntil: 'networkidle2'});
-    await fs.writeFile(imageURL.split("/").pop() , await imagePage.buffer());
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log(" image Download Completed");
+   });
+});
 } catch (error) {
-  console.log(error)
+  
     const videoURL = await page.$eval("source", source => source.src);
 
-    console.log(videoURL + "video downloaded");
-    const videoPage = await page.goto(videoURL, {waitUntil: 'networkidle2'});
-    await fs.writeFile(videoURL.split("/").pop(), await videoPage.buffer());
+    const file = fs.createWriteStream(videoURL.split("/").pop());
+    const request = https.get(videoURL, function(response) {
+      response.pipe(file);
+
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log(" video Download Completed");
+   });
+});
+
 }
 
   await browser.close();

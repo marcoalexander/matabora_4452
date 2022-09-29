15f5dee5b22cdac864ee810f7285efdbb7fbb505
marcoalexander <timmythetom132@gmail.com>
2022-09-28 23:45:31 -0500

    ended up using professors source code and then finished part 3. notice this push does not contain node_modules as last time we tried to push that it broke my git.

---
+++ Project1/myapp/app.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const express = require('express');
+const puppeteer = require('puppeteer');
+const path = require('node:path');
+
+// Constants
+const PORT = 8055;
+const HOST = '0.0.0.0';
+const IMAGE_PATH = 'images';
+const min = 100000;
+const max = 999999;
+
+
+// App
+const app = express();
+// Regular expressions can be used to declare accepted routes in ExpressJS
+// Further reading source: https://expressjs.com/en/guide/routing.html
+app.get('/www.*', (req, res) => {
+  var site = req.url.substring(1);
+  console.log('User requested the following website screenshot: ' + site);
+  site = 'http://' + site;
+  const timestamp = Date.now();
+  // credits: https://stackoverflow.com/a/2175532
+  const random = Math.floor(Math.random() * (max - min + 1)) + min; 
+  const fname = timestamp + "_" + random + ".png";
+  const fpath = path.join(IMAGE_PATH, fname);
+  console.log('Sshot in the file: ' + fpath);
+
+  (async () => {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // If below waitunitl is not added, then the code might take screenshot even before all resources load
+  // networkidle2 is heuristic that "thinks" site is loaded if no more than 2 requests happen in 500 ms;
+  // motivating example: see LinkedIn's network traffic recently!! (they have some tracking code that keeps polling)
+  // Relevant source: https://blog.cloudlayer.io/puppeteer-waituntil-options/
+  await page.goto(site, {waitUntil: 'networkidle2'}); 
+  await page.screenshot({path: fpath});
+  await browser.close();
+  //sendFile() needs absolute path which resolve provides....
+  await res.sendFile(path.resolve(fpath));
+  })();
+
+});
+
+app.get('/kitty', (req, res) => {
+  console.log('User requested a cat image');
+
+  const site = 'https://genrandom.com/cats/';
+  const timestamp = Date.now();
+  // credits: https://stackoverflow.com/a/2175532
+  const random = Math.floor(Math.random() * (max - min + 1)) + min; 
+  const fname = timestamp + "_" + random + ".png";
+  const fpath = path.join(IMAGE_PATH, fname);
+  console.log('Sshot in the file: ' + fpath);
+
+  (async () => {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // If below waitunitl is not added, then the code might take screenshot even before all resources load
+  // networkidle2 is heuristic that "thinks" site is loaded if no more than 2 requests happen in 500 ms;
+  // motivating example: see LinkedIn's network traffic recently!! (they have some tracking code that keeps polling)
+  // Relevant source: https://blog.cloudlayer.io/puppeteer-waituntil-options/
+  //await console.log('going to the page');
+  await page.goto(site, {waitUntil: 'networkidle2'}); 
+  //await console.log('went to the site');
+
+  // Credits: https://www.testim.io/blog/puppeteer-screenshot/
+  await page.waitForSelector('#gatsby-focus-wrapper > div > div > div > div > div > div > div > div > img');
+  const image =  await page.$('#gatsby-focus-wrapper > div > div > div > div > div > div > div > div > img');
+  await image.screenshot({path: fpath});
+  await browser.close();
+  //sendFile() needs absolute path which resolve provides....
+  await res.sendFile(path.resolve(fpath));
+  })();
+
+});
+
+app.get('/puppy', (req, res) => {
+  console.log('User requested a dog image');
+
+  const site = 'https://random.dog/';
+  const timestamp = Date.now();
+  // credits: https://stackoverflow.com/a/2175532
+  const random = Math.floor(Math.random() * (max - min + 1)) + min; 
+  const fname = timestamp + "_" + random + ".png";
+  const fpath = path.join(IMAGE_PATH, fname);
+  console.log('Sshot in the file: ' + fpath);
+   
+  // My cat code failed for the dog site (by giving me blank images;) So, I searched around and found the 
+  // following solution: https://petertran.com.au/2018/07/12/blank-images-puppeteer-screenshots-solved/
+  const defaultViewport = {
+    height: 1920,
+    width: 1280
+  };
+  
+  (async () => {
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // If below waitunitl is not added, then the code might take screenshot even before all resources load
+  // networkidle2 is heuristic that "thinks" site is loaded if no more than 2 requests happen in 500 ms;
+  // motivating example: see LinkedIn's network traffic recently!! (they have some tracking code that keeps polling)
+  // Relevant source: https://blog.cloudlayer.io/puppeteer-waituntil-options/
+  //await console.log('going to the page');
+  await page.goto(site, {waitUntil: 'networkidle2'}); 
+  //await console.log('went to the site');
+
+  // Credits: https://www.testim.io/blog/puppeteer-screenshot/
+  // Note that this selector approach works regardless of what format the images are in!
+  await page.waitForSelector('#dog-img');
+
+  // Resize the viewport to screenshot elements outside of the viewport
+  const bodyHandle = await page.$('body');
+  const boundingBox = await bodyHandle.boundingBox();
+  const newViewport = {
+    width: Math.max(defaultViewport.width, Math.ceil(boundingBox.width)),
+    height: Math.max(defaultViewport.height, Math.ceil(boundingBox.height)),
+  };
+  await page.setViewport(Object.assign({}, defaultViewport, newViewport));
+
+
+  const image =  await page.$('#dog-img');
+  await image.screenshot({path: fpath});
+  await browser.close();
+  //sendFile() needs absolute path which resolve provides....
+  await res.sendFile(path.resolve(fpath));
+  })();
+});
+
+app.all(/.*/, (req, res) => {
+  console.log('User made an error request: ' + req.url);
+  res.send('Hola error');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
--- Project1/webServer.js
+++ Project1/webServer.js
@@ -1,12 +1,107 @@
-const http = require('http')
+/*
+Welcome to my first node js program! this program uses node js and puppeteer to scrape images from a given website,
+and will display it to the local server created.
+*/
+//here im creating all of my objects
+const http = require('http');
+const puppeteer = require('puppeteer');
+const fsp = require('fs/promises');
+const fs = require('fs');
+const https = require('https');
+const prompt = require('prompt-sync')();
 
+//some technical stuff for the server portion of the project
 const hostname = '127.0.0.1'
-const port = 3000
-
+const port = 8055
+//server creation
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  //using if statements to control the url input
+  if (req.url === '/kitty') {
+    //if kitty, website will be scraped of jpg image, saved, then displayed onto server.
+    (async () => {
+  const browser = await puppeteer.launch({headless: false, slowMo: 150,});
+  const page = await browser.newPage();
+  await page.goto('https://genrandom.com/cats/', {waitUntil: 'networkidle2'});
+  //using img as a selector works, but just in case i also used document.querySelector('div[class="css-14qatfq-stack css-3mf84h"] > img') within dev tools in order to find the img
+  const imageURL = await page.$eval("img", img => img.src);
+
+  console.log(imageURL);
+
+  const imagePage = await page.goto(imageURL);
+  await fsp.writeFile("cat.jpg", await imagePage.buffer());
+
+  await browser.close();
+
+})();
+  setTimeout(() => {const readStream = fs.createReadStream('./cat.jpg');
+  res.writeHead(200, {'Content-type': 'image/jpg'});
+  readStream.pipe(res);}, 5000);
+
+  
+
+  }
+  //if puppy
+  if (req.url === '/puppy') {
+    var imageBool = true;
+    //start of webscrape puppy
+    (async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://random.dog', {waitUntil: 'networkidle2'});
+
+  try {
+    //using img as a selector works, but just in case i also used document.querySelector('div[class="css-14qatfq-stack css-3mf84h"] > img') within dev tools in order to find the img
+    const imageURL = await page.$eval("img", img => img.src);
+
+    const file = fs.createWriteStream(imageURL.split("/").pop());
+    const request = https.get(imageURL, function(response) {
+      response.pipe(file);
+
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log(" image Download Completed");
+   });
+   imageBool = true;
+   console.log('imagebool has been set to:' + imageBool);
+});
+} catch (error) {
+    imageBool = false;
+    console.log('imagebool has been set to:' + imageBool);
+    const videoURL = await page.$eval("source", source => source.src);
+
+    const file = fs.createWriteStream(videoURL.split("/").pop());
+    const request = https.get(videoURL, function(response) {
+      response.pipe(file);
+
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log(" video Download Completed");
+   });
+});
+
+}
+
+  await browser.close();
+})();//end of webscrape puppy
+console.log('boolean check after webscrape, trying to display image: imagebool has been set to:' + imageBool);
+  if (imageBool) {setTimeout(() => {const readStream = fs.createReadStream('./' + imageURL.split("/").pop());
+  res.writeHead(200, {'Content-type': 'image'});
+  readStream.pipe(res);}, 7000);
+  }else{
+    setTimeout(() => {const readStream = fs.createReadStream('./' + videoURL.split("/").pop());
+  res.writeHead(200, {'Content-type': 'video/mp4'});
+  readStream.pipe(res);}, 7000);
+  }
+  
+  }//end of puppy url
+
+
+  //if anything else
+  if (req.url === 'www') {
+    
+  }
 })
 
 server.listen(port, hostname, () => {
